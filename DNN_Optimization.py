8ad63c37ca1b96e21c16220636249f43651a4659
hind77 <hind.boukhairat@gmail.com>
2021-01-18 14:15:42 +0100

    zero padding + LeakyReLu

--- DNN_Optimization.py
+++ DNN_Optimization.py
@@ -20,6 +20,7 @@ import seaborn as sns
 import math 
 from scipy import special as sp
 from tensorflow.keras.layers import BatchNormalization
+from keras.layers import LeakyReLU
 import matplotlib.pyplot as plt
 from keras import layers
 from keras import models
@@ -218,16 +219,20 @@ def choose_model(init_model, choice):
             model._name = 'Dropout_Model_Rl1'
  
     if choice == "dropout_Rl2":           
-            model.add(Dense(kernel_initializer=initializer, activation='relu'))
+            model.add(Dense(kernel_initializer=initializer))
+            model.add(LeakyReLU(alpha=0.01))
             model.add(layers.Dropout(0.3))
             model.add(BatchNormalization())
-            model.add(Dense(64, kernel_regularizer=regularizers.l2(0.0005), activation='relu'))
+            model.add(Dense(64, kernel_regularizer=regularizers.l2(0.0005)))
+            model.add(LeakyReLU(alpha=0.01))
             model.add(layers.Dropout(0.3))
             model.add(BatchNormalization())
-            model.add(Dense(32, kernel_regularizer=regularizers.l2(0.0005), activation='relu'))
+            model.add(Dense(32, kernel_regularizer=regularizers.l2(0.0005)))
+            model.add(LeakyReLU(alpha=0.01))
             model.add(layers.Dropout(0.3))
             model.add(BatchNormalization())
-            model.add(Dense(16, kernel_regularizer=regularizers.l2(0.0005), activation='relu'))
+            model.add(Dense(16, kernel_regularizer=regularizers.l2(0.0005)))
+            model.add(LeakyReLU(alpha=0.01))
             model.add(layers.Dropout(0.3))
             model.add(BatchNormalization())
             model.add(Dense(num_sens, activation='softmax',name='last_layer'))
